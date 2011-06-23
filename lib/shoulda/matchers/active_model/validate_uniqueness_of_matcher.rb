0001870a247a7a58acd50db4d918af0129e66768
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-06-24 02:47:36 +0800

    Use #first instead of #find(:first) which is not available in Mongoid, MongoMapper and DataMapper

--- lib/shoulda/matchers/active_model/validate_uniqueness_of_matcher.rb
+++ lib/shoulda/matchers/active_model/validate_uniqueness_of_matcher.rb
@@ -76,7 +76,7 @@ module Shoulda # :nodoc:
         private
 
         def find_existing
-          if @existing = @subject.class.find(:first)
+          if @existing = @subject.class.first
             true
           else
             @failure_message = "Can't find first #{class_name}"
